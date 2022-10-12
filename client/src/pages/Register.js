a131eb8771e719b341d84ce002d1fc1c23c1c456
BartoszJS <bartosz290300@wp.pl>
2022-10-12 20:42:42 +0200

    16 redirect register

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import { useAppContext } from "../context/appContext";
+import { useNavigate } from "react-router-dom";
 
 const initialState = {
   name: "",
@@ -11,43 +12,60 @@ const initialState = {
 };
 
 const Register = () => {
+  const navigate = useNavigate();
   const [values, setValues] = useState(initialState);
-  const {isLoading,showAlert,displayAlert} = useAppContext();
+  const { user, isLoading, showAlert, displayAlert, registerUser } =
+    useAppContext();
 
-  const toggleMember = () =>{
-    setValues({...values, isMember:!values.isMember})
-  }
- 
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
   //global state and use navigate
 
   const handleChange = (e) => {
-    setValues({...values,[e.target.name]: e.target.value})
-  }
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const {name,email,password,isMember} = values
-    if(!email || !password || (!isMember && !name)){
-      displayAlert()
-      return
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
     }
-    console.log(values);
-  }
+    const currentUser = { name, email, password };
+    if (isMember) {
+      console.log("already a member");
+    } else {
+      registerUser(currentUser);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate("/");
+      }, 1500);
+     
+    }
+  }, [user, navigate]);
+
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>{values.isMember ? "Login" : 'Register'}</h3>
-        {showAlert && <Alert alertText='yasss'/>}
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {showAlert && <Alert alertText="yasss" />}
         {!values.isMember && (
           <FormRow
-          type="text"
-          name="name"
-          value={values.name}
-          handleChange={handleChange}
-        />
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
         )}
-        
+
         <FormRow
           type="email"
           name="email"
@@ -60,13 +78,13 @@ const Register = () => {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type="submit" className="btn btn-block">
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
           submit
         </button>
         <p>
-        {values.isMember ? 'Not a member?' : 'Already a member?'}
-          <button type='button' onClick={toggleMember} className='member-btn'>
-            {values.isMember ? 'Register' : 'Login'}
+          {values.isMember ? "Not a member?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
           </button>
         </p>
       </form>
