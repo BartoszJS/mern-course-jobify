a01145d8b925cceb51c0637d9ef9f26a16c47add
BartoszJS <bartosz290300@wp.pl>
2022-10-13 23:52:03 +0200

    21 sidebar navbarr

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/BigSidebar'
+import React from "react";
+import Wrapper from "../assets/wrappers/BigSidebar";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-    <h1>BigSidebar</h1>
+      <div
+        className={
+          showSidebar ? "sidebar-container " : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default BigSidebar
+export default BigSidebar;
--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/SmallSidebar'
+import React from "react";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-        <h1> Small sidebar</h1>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar}/>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default SmallSidebar
+export default SmallSidebar;
