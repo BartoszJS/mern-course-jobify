9dc7903a39a2d7239ef8da7cd9708577097f8f2b
BartoszJS <bartosz290300@wp.pl>
2022-10-13 19:12:19 +0200

    19 sidebar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <button
+          className="toggle-btn"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("show/hide dropdown")}
+          >
+            <FaUserCircle />
+            john
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              className="dropdown-btn"
+              type="button"
+              onClick={() => console.log("logout user")}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Navbar;
