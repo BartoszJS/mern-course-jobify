eed7959990874f025f62d2cb4d144ce2ade78649
BartoszJS <bartosz290300@wp.pl>
2022-10-13 15:58:53 +0200

    18 routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,35 @@
-import {Error, Dashboard, Register, Landing} from './pages/index'
+import { Error, Register, Landing, ProtectedRoute } from "./pages/index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  SharedLayout,
+  Profile,
+  Stats,
+  AllJobs,
+  AddJob,
+} from "./pages/dashboard";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-      
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/landing" element={<Landing />} />
-          <Route path="*" element={<Error />} />
-          
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
