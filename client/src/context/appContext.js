12d94e68d06e2469d48ef37302b2715f848ff13b
BartoszJS <bartosz290300@wp.pl>
2022-10-17 14:06:27 +0200

    27 edit delete

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -23,6 +23,10 @@ import {
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
   SET_EDIT_JOB,
+  DELETE_JOB_BEGIN,
+  EDIT_JOB_BEGIN,
+  EDIT_JOB_ERROR,
+  EDIT_JOB_SUCCESS,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -226,19 +230,44 @@ const AppProvider = ({ children }) => {
       console.log(error.response);
       //logoutUser()
     }
-    clearAlert()
+    clearAlert();
   };
 
-  const setEditJob = (id) =>{
-    dispatch({type: SET_EDIT_JOB,payload:{id}})
-  }
-  const editJob = () =>{
-    console.log('edit job');
-  }
-  const deleteJob = (id) =>{
-    console.log(`delete job:${id}`);
-  }
-
+  const setEditJob = (id) => {
+    dispatch({ type: SET_EDIT_JOB, payload: { id } });
+  };
+  const editJob = async () => {
+    dispatch({ type: EDIT_JOB_BEGIN });
+    try {
+      const { position, company, jobLocation, jobType, status } = state;
+      await authFetch.patch(`/jobs/${state.editJobId}`, {
+        company,
+        position,
+        jobLocation,
+        jobType,
+        status,
+      });
+      dispatch({type: EDIT_JOB_SUCCESS})
+      dispatch({ type: CLEAR_VALUES });
+    } catch (error) {
+      if (error.response.status === 401) return;
+      dispatch({
+        type: EDIT_JOB_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+    clearAlert()
+  };
+  const deleteJob = async (jobId) => {
+    dispatch({ type: DELETE_JOB_BEGIN });
+    try {
+      await authFetch.delete(`/jobs/${jobId}`);
+      getJobs();
+    } catch (error) {
+      console.log(error.response);
+      //logoutUser()
+    }
+  };
 
   return (
     <AppContext.Provider
